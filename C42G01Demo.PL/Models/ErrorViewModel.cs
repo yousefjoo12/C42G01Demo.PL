b7a607b665263ad7231c074e41f2ed79510771a0
Youssef <yousefxbxd12@gmail.com>
2024-09-06 15:51:22 +0300

    Add project files.

---
+++ C42G01Demo.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace C42G01Demo.PL.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
