a77772904e02061ed757fbff7eefe536ad2c58e1
Youssef <yousefxbxd12@gmail.com>
2024-09-23 08:06:06 +0300

    update

---
+++ C42G01Demo.PL/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using C42G01Demo.PL.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace C42G01Demo.PL.Controllers
+{
+	public class AccountController : Controller
+	{
+		#region Sign Up
+		public IActionResult SignUp()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult SignUp(SignUpViewModel ViewModel)
+		{
+			if (ModelState.IsValid) 
+			{
+				var user = new IdentityUser()
+				{
+
+				};
+			}
+			return View(ViewModel);
+		}
+		#endregion
+	}
+}
---
+++ C42G01Demo.PL/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C42G01Demo.PL.ViewModels
+{
+	public class SignUpViewModel
+	{
+		[Required(ErrorMessage = "Email Is Required")]
+		[EmailAddress(ErrorMessage ="IsValid Email")]
+		public string Email { get; set; }
+
+		[Required(ErrorMessage = "Password Is Required")]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+
+		[Required(ErrorMessage = "Confirm Password Is Required")]
+		[Compare(nameof(Password), ErrorMessage = "Confirm Password dose't match Password")]
+		public string ConfirmPassword { get; set; }
+
+		[Required(ErrorMessage = "Required To Agree")]
+		public bool IsAgree { get; set; }
+
+
+	}
+}
